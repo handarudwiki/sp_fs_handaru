66d31740902ffe0af7615193c35d30f85fe025cf
handarudwiki <handarudwiki04@gmail.com>
2025-07-01 00:13:25 +0700

    fix: await in register

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -5,7 +5,7 @@ import successResponse from "../helpers/success_response";
 export default class UserController {
     static async register(req:Request, res:Response, next:NextFunction) {
         try {
-            const result = UserService.register(req.body);
+            const result = await UserService.register(req.body);
             
              successResponse(res, result, "User registered successfully");
         } catch (error) {
